3db5079eac5afba8f57ac63405834ee899860823
Maria <mariaszpc@gmail.com>
2026-05-17 15:39:42 -0300

    Criação de Dashboard como página inicial

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
 from .models import Linguagem
 
+# Views da Wiki
 class LinguagemListView(ListView):
     model = Linguagem
     template_name = 'core/linguagem_list.html'
@@ -10,3 +11,19 @@ class LinguagemDetailView(DetailView):
     model = Linguagem
     template_name = 'core/linguagem_detail.html'
     context_object_name = 'linguagem'
+
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Linguagem, Vaga, Aplicacao
+
+# Dashboard
+class DashboardView(TemplateView):
+    template_name = 'core/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Pegando a quantidade de itens no banco de dados para mostrar nos Cards
+        context['total_linguagens'] = Linguagem.objects.count()
+        context['total_vagas'] = Vaga.objects.count()
+        # Conta aplicações que não estejam com status "Negado" (etapa do funil)
+        context['aplicacoes_ativas'] = Aplicacao.objects.exclude(etapa_atual='Proposta').count()
+        return context
--- techtrack/urls.py
+++ techtrack/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import LinguagemListView, LinguagemDetailView 
+from core.views import LinguagemListView, LinguagemDetailView, DashboardView 
 
 urlpatterns = [
     # Rota do Painel Administrativo
     path("admin/", admin.site.urls),
+    
+    # Rota inicial vazia apontando para o Dashboard:
+    path('', DashboardView.as_view(), name='dashboard'),
     
     # Rotas da Wiki
     path('wiki/', LinguagemListView.as_view(), name='linguagem_list'),
